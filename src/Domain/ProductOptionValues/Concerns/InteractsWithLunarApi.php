5e69f142311d0f4e1bc0489306980dd9f4405383
Jakub Theimer <5587309+theimerj@users.noreply.github.com>
2024-10-29 13:29:29 +0100

    add images relation

--- src/Domain/ProductOptionValues/Concerns/InteractsWithLunarApi.php
+++ src/Domain/ProductOptionValues/Concerns/InteractsWithLunarApi.php
@@ -3,7 +3,10 @@
 namespace Dystcz\LunarApi\Domain\ProductOptionValues\Concerns;
 
 use Dystcz\LunarApi\Domain\ProductOptionValues\Factories\ProductOptionValueFactory;
+use Dystcz\LunarApi\Domain\ProductOptionValues\Models\ProductOptionValue;
 use Dystcz\LunarApi\Hashids\Traits\HashesRouteKey;
+use Illuminate\Database\Eloquent\Relations\MorphMany;
+use Illuminate\Support\Facades\Config;
 
 trait InteractsWithLunarApi
 {
@@ -15,5 +18,17 @@ trait InteractsWithLunarApi
     protected static function newFactory(): ProductOptionValueFactory
     {
         return ProductOptionValueFactory::new();
+    }
+
+    public function images(): MorphMany
+    {
+        /** @var ProductOptionValue $this */
+
+        return $this
+            ->media()
+            ->where(
+                'collection_name',
+                Config::get('lunar.media.collection'),
+            );
     }
 }
